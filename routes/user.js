ce04e596d43edc7072cc830be178c2335d73909a
Mariangel Yajure <mariangel.yajure@gmail.com>
2022-08-15 10:50:39 -0430

    Validando los roles con la base de datos

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,9 @@
 const {Router} = require('express');
 const {check} = require('express-validator');
+const Role = require('../models/role');
+
 const {fieldValidations} = require('../middlewares/field-validations')
+
 const {usersGet, 
 	usersPost,
 	usersPut,
@@ -13,10 +16,19 @@ const router = Router();
 router.get('/', usersGet);
 
 router.post('/', [
+	
 	check('name', 'El nombre es obligatorio').not().isEmpty(),
 	check('password', 'La contraseña debe contener más de 6 carácteres').isLength({min: 6}),
 	check('email', 'El correo no es válido').isEmail(),
-	check('role', 'El rol no es válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+	// check('role', 'El rol no es válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+	
+	check('role').custom(async(role = '') => {
+		const rolExists = await Role.findOne({role});
+		if(!rolExists){
+			throw new Error(`El rol ${role} no está registrado en la base de datos`);
+		}
+	}),
+	
 	fieldValidations
 ], usersPost);
 
